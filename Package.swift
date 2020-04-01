d5ce4aa30d8db2a854a5cd57b3cda671cb1828d9
Joseph Duffy <commits@josephduffy.co.uk>
2020-04-02 00:26:15 +0100

    Set supported platform versions

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Persist",
+    platforms: [
+        .iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2),
+    ],
     products: [
         .library(name: "Persist", targets: ["Persist"]),
     ],
