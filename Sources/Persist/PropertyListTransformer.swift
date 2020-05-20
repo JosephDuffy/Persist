532e4a9efb9c8c6585899ea2fe0bbc40b54d7457
Joseph Duffy <commits@josephduffy.co.uk>
2020-05-20 21:15:09 +0100

    Add `PropertyListTransformer`

---
+++ Sources/Persist/PropertyListTransformer.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+public struct PropertyListTransformer<Input: Codable>: Transformer {
+
+    public typealias Output = Data
+
+    public var encoder: PropertyListEncoder
+
+    public var decoder: PropertyListDecoder
+
+    public init(encoder: PropertyListEncoder = PropertyListEncoder(), decoder: PropertyListDecoder = PropertyListDecoder()) {
+        self.encoder = encoder
+        self.decoder = decoder
+    }
+
+    public init(outputFormat: PropertyListSerialization.PropertyListFormat) {
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = outputFormat
+
+        self.init(encoder: encoder)
+    }
+
+    public init(coderUserInfo userInfo: [CodingUserInfoKey: Any]) {
+        let encoder = PropertyListEncoder()
+        let decoder = PropertyListDecoder()
+        encoder.userInfo = userInfo
+        decoder.userInfo = userInfo
+
+        self.init(encoder: encoder, decoder: decoder)
+    }
+
+    public init(outputFormat: PropertyListSerialization.PropertyListFormat, userInfo: [CodingUserInfoKey: Any]) {
+        let encoder = PropertyListEncoder()
+        let decoder = PropertyListDecoder()
+        encoder.outputFormat = outputFormat
+        encoder.userInfo = userInfo
+        decoder.userInfo = userInfo
+
+        self.init(encoder: encoder, decoder: decoder)
+    }
+
+    public func transformValue(_ value: Input) throws -> Data {
+        return try encoder.encode(value)
+    }
+
+    public func untransformValue(from data: Data) throws -> Input {
+        return try decoder.decode(Input.self, from: data)
+    }
+
+}
