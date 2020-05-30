c032842c0e2d452f35e4185e1355034c4f7e5587
Joseph Duffy <commits@josephduffy.co.uk>
2020-05-30 22:21:01 +0100

    Add `JSONTransformerTests`

---
+++ Tests/PersistTests/JSONTransformerTests.swift
@@ -0,0 +1,19 @@
+#if !os(watchOS)
+import XCTest
+@testable import Persist
+
+final class JSONTransformerTests: XCTestCase {
+
+    func testJSONTransformerWithCoderUserInfo() {
+        let customKey = CodingUserInfoKey(rawValue: "test-key")!
+        let customKeyValue = "the-value"
+        let coderUserInfo: [CodingUserInfoKey: Any] = [
+            customKey: customKeyValue,
+        ]
+        let transformer = JSONTransformer<String>(coderUserInfo: coderUserInfo)
+        XCTAssertEqual(transformer.encoder.userInfo[customKey] as? String, customKeyValue)
+        XCTAssertEqual(transformer.decoder.userInfo[customKey] as? String, customKeyValue)
+    }
+
+}
+#endif
