10b6fdc990a314151ae24d2f92142b7b4762d924
Joseph Duffy <commits@josephduffy.co.uk>
2020-05-30 19:49:31 +0100

    Wrap `StoredInUserDefaults` in `if os`

--- Sources/Persist/UserDefaults/StoredInUserDefaults.swift
+++ Sources/Persist/UserDefaults/StoredInUserDefaults.swift
@@ -1,4 +1,6 @@
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 import Foundation
 
 /// A propety wrapper that stores a value in `UserDefaults`.
 public typealias StoredInUserDefaults<Value: StorableInUserDefaults> = Persisted<Value>
+#endif
