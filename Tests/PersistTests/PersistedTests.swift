5cc7332d8da69c4d4e247ac03f85fe0da9cb6047
Joseph Duffy <commits@josephduffy.co.uk>
2020-05-30 23:10:49 +0100

    Add more Persisted tests

--- Tests/PersistTests/PersistedTests.swift
+++ Tests/PersistTests/PersistedTests.swift
@@ -104,5 +104,256 @@ final class PersistedTests: XCTestCase {
         waitForExpectations(timeout: 1, handler: nil)
     }
 
+    func testSettingWrappedValueToNilWithTransformer() throws {
+        struct StoredValue: Codable, Equatable {
+            let property: String
+        }
+        var persisted = Persisted<StoredValue>(key: "test-key", storedBy: InMemoryStorage(), transformer: JSONTransformer())
+        let storedValue = StoredValue(property: "value")
+        persisted.wrappedValue = storedValue
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persisted.projectedValue.addUpdateListener { result in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch result {
+            case .success(let newValue):
+                XCTAssertNil(newValue, "Value passed to update listener should be nil to indicate delete")
+            case .failure(let error):
+                XCTFail("Update listener should be notified of a success. Got error: \(error)")
+            }
+        }
+        _ = cancellable
+
+        persisted.wrappedValue = nil
+
+        XCTAssertNil(persisted.wrappedValue, "Should return nil when value has been deleted")
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func testAnyStorageSettingWithDifferentStoredValueType() throws {
+        let key = "key"
+        let actualValue = "test"
+        let storage = InMemoryStorage<Any>()
+        let persister = Persister<Int>(key: key, storedBy: storage)
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persister.addUpdateListener() { newValue in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch newValue {
+            case .failure(PersistenceError.unexpectedValueType(let value, let expected)):
+                XCTAssertEqual(value as? String, actualValue)
+                XCTAssert(expected == Int.self)
+            default:
+                XCTFail()
+            }
+        }
+        _ = cancellable
+
+        storage.storeValue(actualValue, key: "key")
+
+        XCTAssertThrowsError(try persister.retrieveValue(), "Retrieving a value with a different type should throw") { error in
+            switch error {
+            case PersistenceError.unexpectedValueType(let value, let expected):
+                XCTAssertEqual(value as? String, actualValue)
+                XCTAssert(expected == Int.self)
+            default:
+                XCTFail()
+            }
+        }
+
+        waitForExpectations(timeout: 0.1)
+    }
+
+    func testAnyStorageSettingWithDifferentStoredValueTypeAndTransformerOutput() throws {
+        let key = "key"
+        let actualValue = "test"
+        let storage = InMemoryStorage<Any>()
+        let persister = Persister<Int>(key: key, storedBy: storage, transformer: JSONTransformer())
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persister.addUpdateListener() { newValue in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch newValue {
+            case .failure(PersistenceError.unexpectedValueType(let value, let expected)):
+                XCTAssertEqual(value as? String, actualValue)
+                XCTAssert(expected == Data.self)
+            default:
+                XCTFail()
+            }
+        }
+        _ = cancellable
+
+        storage.storeValue(actualValue, key: "key")
+
+        XCTAssertThrowsError(try persister.retrieveValue(), "Retrieving a value with a different type should throw") { error in
+            switch error {
+            case PersistenceError.unexpectedValueType(let value, let expected):
+                XCTAssertEqual(value as? String, actualValue)
+                XCTAssert(expected == Data.self)
+            default:
+                XCTFail()
+            }
+        }
+
+        waitForExpectations(timeout: 0.1)
+    }
+
+    func testAnyStorageSettingWithDifferentStoredValueTypeAndUntransformedValue() {
+        let key = "key"
+        let actualValue = "test"
+        let storage = InMemoryStorage<Any>()
+        let persister = Persister<Int>(key: key, storedBy: storage, transformer: StorableInUserDefaultsTransformer())
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persister.addUpdateListener() { newValue in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch newValue {
+            case .failure(PersistenceError.unexpectedValueType(let value, let expected)):
+                XCTAssertEqual(value as? String, actualValue)
+                XCTAssert(expected == Int.self)
+            default:
+                XCTFail()
+            }
+        }
+        _ = cancellable
+
+        storage.storeValue(UserDefaultsValue.string(actualValue), key: key)
+
+        XCTAssertThrowsError(try persister.retrieveValue(), "Retrieving a value with a different type should throw") { error in
+            switch error {
+            case PersistenceError.unexpectedValueType(let value, let expected):
+                XCTAssertEqual(value as? String, actualValue)
+                XCTAssert(expected == Int.self)
+            default:
+                XCTFail()
+            }
+        }
+
+        waitForExpectations(timeout: 0.1)
+    }
+
+    func testAnyStorageSettingWithoutTransformer() throws {
+        struct StoredValue: Codable, Equatable {
+            let property: String
+        }
+        var persisted = Persisted<StoredValue>(key: "test-key", storedBy: InMemoryStorage<Any>())
+        let storedValue = StoredValue(property: "value")
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persisted.projectedValue.addUpdateListener { result in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch result {
+            case .success(let newValue):
+                XCTAssertEqual(newValue, storedValue, "Value passed to update listener should be the new value")
+            case .failure(let error):
+                XCTFail("Update listener should be notified of a success. Got error: \(error)")
+            }
+        }
+        _ = cancellable
+
+        persisted.wrappedValue = storedValue
+        XCTAssertEqual(persisted.wrappedValue, storedValue, "Should return untransformed value")
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func testAnyStorageDeletingWithoutTransformer() throws {
+        struct StoredValue: Codable, Equatable {
+            let property: String
+        }
+        var persisted = Persisted<StoredValue>(key: "test-key", storedBy: InMemoryStorage<Any>())
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persisted.projectedValue.addUpdateListener { result in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch result {
+            case .success(let newValue):
+                XCTAssertNil(newValue, "Value passed to update listener should be nil to indicate delete")
+            case .failure(let error):
+                XCTFail("Update listener should be notified of a success. Got error: \(error)")
+            }
+        }
+        _ = cancellable
+
+        persisted.wrappedValue = nil
+        XCTAssertNil(persisted.wrappedValue, "Should return nil when value has been deleted")
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func testAnyStorageSettingWithTransformer() throws {
+        struct StoredValue: Codable, Equatable {
+            let property: String
+        }
+        var persisted = Persisted<StoredValue>(key: "test-key", storedBy: InMemoryStorage<Any>(), transformer: JSONTransformer())
+        let storedValue = StoredValue(property: "value")
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persisted.projectedValue.addUpdateListener { result in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch result {
+            case .success(let newValue):
+                XCTAssertEqual(newValue, storedValue, "Value passed to update listener should be the new value")
+            case .failure(let error):
+                XCTFail("Update listener should be notified of a success. Got error: \(error)")
+            }
+        }
+        _ = cancellable
+
+        persisted.wrappedValue = storedValue
+        XCTAssertEqual(persisted.wrappedValue, storedValue, "Should return untransformed value")
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func testAnyStorageDeletingWithTransformer() throws {
+        struct StoredValue: Codable, Equatable {
+            let property: String
+        }
+        var persisted = Persisted<StoredValue>(key: "test-key", storedBy: InMemoryStorage<Any>(), transformer: JSONTransformer())
+
+        let callsUpdateListenerExpectation = expectation(description: "Calls update listener")
+        let cancellable = persisted.projectedValue.addUpdateListener { result in
+            defer {
+                callsUpdateListenerExpectation.fulfill()
+            }
+
+            switch result {
+            case .success(let newValue):
+                XCTAssertNil(newValue, "Value passed to update listener should be nil to indicate delete")
+            case .failure(let error):
+                XCTFail("Update listener should be notified of a success. Got error: \(error)")
+            }
+        }
+        _ = cancellable
+
+        persisted.wrappedValue = nil
+        XCTAssertNil(persisted.wrappedValue, "Should return nil when value has been deleted")
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
 }
 #endif
