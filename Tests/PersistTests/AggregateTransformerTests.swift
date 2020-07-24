651ba0b550078e883070174f2fec0b0ec13d98ca
Joseph Duffy <commits@josephduffy.co.uk>
2020-07-24 17:51:07 +0100

    Add `AggregateTransformer` tests

---
+++ Tests/PersistTests/AggregateTransformerTests.swift
@@ -0,0 +1,110 @@
+#if !os(watchOS)
+import XCTest
+@testable import Persist
+
+final class AggregateTransformerTests: XCTestCase {
+
+    func testTransformValue() {
+        let firstTransformer = MockTransformer<String>()
+        firstTransformer.transformedValue = "first-transformed"
+        let secondTransformer = MockTransformer<String>()
+        secondTransformer.transformedValue = "second-transformed"
+
+        let aggregate = firstTransformer.append(transformer: secondTransformer)
+        let input = "input"
+        let transformed = try? aggregate.transformValue(input)
+
+        XCTAssert(firstTransformer.transformValueParameter == input, "Input should be passed to first transformer")
+        XCTAssertNotNil(secondTransformer.transformValueParameter, "Second transformer should be called")
+        XCTAssert(secondTransformer.transformValueParameter == firstTransformer.transformedValue, "Output from first transformer should be passed to second transformer")
+        XCTAssertNotNil(transformed, "Should return a value")
+        XCTAssert(transformed == secondTransformer.transformedValue, "Should return output of second transformer")
+    }
+
+    func testUntransformValue() {
+        let firstTransformer = MockTransformer<String>()
+        firstTransformer.untransformedValue = "first-untransformed"
+        let secondTransformer = MockTransformer<String>()
+        secondTransformer.untransformedValue = "second-untransformed"
+
+        let aggregate = firstTransformer.append(transformer: secondTransformer)
+        let output = "output"
+        let untransformed = try? aggregate.untransformValue(output)
+
+        XCTAssert(secondTransformer.untransformValueParameter == output, "Input should be passed to first transformer")
+        XCTAssertNotNil(firstTransformer.untransformValueParameter, "Second transformer should be called")
+        XCTAssert(firstTransformer.untransformValueParameter == secondTransformer.untransformedValue, "Output from first transformer should be passed to second transformer")
+        XCTAssertNotNil(untransformed, "Should return a value")
+        XCTAssert(untransformed == firstTransformer.untransformedValue, "Should return output of second transformer")
+    }
+
+    func testTransformValueFirstTransformerThrows() {
+        let firstTransformer = MockTransformer<String>()
+        let secondTransformer = MockTransformer<String>()
+        let thrownError = NSError(domain: "persist.tests", code: -1, userInfo: nil)
+        firstTransformer.errorToThrow = thrownError
+
+        let aggregate = firstTransformer.append(transformer: secondTransformer)
+        let input = "input"
+        XCTAssertThrowsError(
+            try aggregate.transformValue(input),
+            "Should throw when transformer throws",
+            { error in
+                XCTAssertEqual(error as NSError, thrownError, "Should throw error thrown by transformer")
+            }
+        )
+    }
+
+    func testTransformValueSecondTransformerThrows() {
+        let firstTransformer = MockTransformer<String>()
+        let secondTransformer = MockTransformer<String>()
+        let thrownError = NSError(domain: "persist.tests", code: -1, userInfo: nil)
+        secondTransformer.errorToThrow = thrownError
+
+        let aggregate = firstTransformer.append(transformer: secondTransformer)
+        let input = "input"
+        XCTAssertThrowsError(
+            try aggregate.transformValue(input),
+            "Should throw when transformer throws",
+            { error in
+                XCTAssertEqual(error as NSError, thrownError, "Should throw error thrown by transformer")
+            }
+        )
+    }
+
+    func testUntransformValueFirstTransformerThrows() {
+        let firstTransformer = MockTransformer<String>()
+        let secondTransformer = MockTransformer<String>()
+        let thrownError = NSError(domain: "persist.tests", code: -1, userInfo: nil)
+        firstTransformer.errorToThrow = thrownError
+
+        let aggregate = firstTransformer.append(transformer: secondTransformer)
+        let output = "output"
+        XCTAssertThrowsError(
+            try aggregate.untransformValue(output),
+            "Should throw when transformer throws",
+            { error in
+                XCTAssertEqual(error as NSError, thrownError, "Should throw error thrown by transformer")
+            }
+        )
+    }
+
+    func testUntransformValueSecondTransformerThrows() {
+        let firstTransformer = MockTransformer<String>()
+        let secondTransformer = MockTransformer<String>()
+        let thrownError = NSError(domain: "persist.tests", code: -1, userInfo: nil)
+        secondTransformer.errorToThrow = thrownError
+
+        let aggregate = firstTransformer.append(transformer: secondTransformer)
+        let output = "output"
+        XCTAssertThrowsError(
+            try aggregate.untransformValue(output),
+            "Should throw when transformer throws",
+            { error in
+                XCTAssertEqual(error as NSError, thrownError, "Should throw error thrown by transformer")
+            }
+        )
+    }
+
+}
+#endif
