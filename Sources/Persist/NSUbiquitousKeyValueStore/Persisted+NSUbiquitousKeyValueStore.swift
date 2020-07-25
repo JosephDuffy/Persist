c9c257fa9879a473f3c10cbc7f46726d43b1240b
Joseph Duffy <commits@josephduffy.co.uk>
2020-07-25 22:13:12 +0100

    Allow setting default value via wrapped property
    Support for NSUbiquitousKeyValueStore API

--- Sources/Persist/NSUbiquitousKeyValueStore/Persisted+NSUbiquitousKeyValueStore.swift
+++ Sources/Persist/NSUbiquitousKeyValueStore/Persisted+NSUbiquitousKeyValueStore.swift
@@ -30,6 +30,29 @@ extension Persisted where Value: StorableInNSUbiquitousKeyValueStore {
 
     /**
      Create a new instance that stores the value against the `key`, storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init(
+        wrappedValue: Value,
+        key: String,
+        storedBy nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) {
+        self.init(
+            key: key,
+            nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore,
+            defaultValue: wrappedValue,
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`, storing values in the specified
      `NSUbiquitousKeyValueStore`, defaulting to `defaultValue`.
 
      - parameter key: The key to store the value against
@@ -48,6 +71,30 @@ extension Persisted where Value: StorableInNSUbiquitousKeyValueStore {
             storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
             transformer: StorableInNSUbiquitousKeyValueStoreTransformer<Value>(),
             defaultValue: defaultValue(),
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`, storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init(
+        wrappedValue: Value,
+        key: String,
+        nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) {
+        self.init(
+            key: key,
+            storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
+            transformer: StorableInNSUbiquitousKeyValueStoreTransformer<Value>(),
+            defaultValue: wrappedValue,
             defaultValuePersistBehaviour: defaultValuePersistBehaviour
         )
     }
@@ -83,6 +130,29 @@ extension Persisted {
 
     /**
      Create a new instance that stores the value against the `key`, storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init<WrappedValue>(
+        wrappedValue: Value,
+        key: String,
+        storedBy nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) where WrappedValue: StorableInNSUbiquitousKeyValueStore, Value == WrappedValue? {
+        self.init(
+            key: key,
+            nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore,
+            defaultValue: wrappedValue,
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`, storing values in the specified
      `NSUbiquitousKeyValueStore`, defaulting to `defaultValue`.
 
      - parameter key: The key to store the value against
@@ -101,6 +171,30 @@ extension Persisted {
             storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
             transformer: StorableInNSUbiquitousKeyValueStoreTransformer<WrappedValue>(),
             defaultValue: defaultValue(),
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`, storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init<WrappedValue>(
+        wrappedValue: Value,
+        key: String,
+        nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) where WrappedValue: StorableInNSUbiquitousKeyValueStore, Value == WrappedValue? {
+        self.init(
+            key: key,
+            storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
+            transformer: StorableInNSUbiquitousKeyValueStoreTransformer<WrappedValue>(),
+            defaultValue: wrappedValue,
             defaultValuePersistBehaviour: defaultValuePersistBehaviour
         )
     }
@@ -138,6 +232,35 @@ extension Persisted {
 
     /**
      Create a new instance that stores the value against the `key`,  storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     Values stored will be processed by the provided transformer before being persisted and after being
+     retrieved from the storage.
+
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter transformer: A transformer to transform the value before being persisted and after being retrieved from the storage
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init<Transformer: Persist.Transformer>(
+        wrappedValue: Value,
+        key: String,
+        storedBy nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        transformer: Transformer,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) where Transformer.Input == Value, Transformer.Output: StorableInNSUbiquitousKeyValueStore {
+        self.init(
+            key: key,
+            nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore,
+            transformer: transformer,
+            defaultValue: wrappedValue,
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`,  storing values in the specified
      `NSUbiquitousKeyValueStore`, defaulting to `defaultValue`.
 
      Values stored will be processed by the provided transformer before being persisted and after being
@@ -162,6 +285,36 @@ extension Persisted {
             storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
             transformer: aggregateTransformer,
             defaultValue: defaultValue(),
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`,  storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     Values stored will be processed by the provided transformer before being persisted and after being
+     retrieved from the storage.
+
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter transformer: A transformer to transform the value before being persisted and after being retrieved from the storage
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init<Transformer: Persist.Transformer>(
+        wrappedValue: Value,
+        key: String,
+        nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        transformer: Transformer,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) where Transformer.Input == Value, Transformer.Output: StorableInNSUbiquitousKeyValueStore {
+        let aggregateTransformer = transformer.append(transformer: StorableInNSUbiquitousKeyValueStoreTransformer<Transformer.Output>())
+        self.init(
+            key: key,
+            storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
+            transformer: aggregateTransformer,
+            defaultValue: wrappedValue,
             defaultValuePersistBehaviour: defaultValuePersistBehaviour
         )
     }
@@ -199,6 +352,35 @@ extension Persisted {
 
     /**
      Create a new instance that stores the value against the `key`,  storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     Values stored will be processed by the provided transformer before being persisted and after being
+     retrieved from the storage.
+
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter transformer: A transformer to transform the value before being persisted and after being retrieved from the storage
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init<Transformer: Persist.Transformer, WrappedValue>(
+        wrappedValue: Value,
+        key: String,
+        storedBy nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        transformer: Transformer,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) where Transformer.Input == WrappedValue, Transformer.Output: StorableInNSUbiquitousKeyValueStore, Value == WrappedValue? {
+        self.init(
+            key: key,
+            nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore,
+            transformer: transformer,
+            defaultValue: wrappedValue,
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`,  storing values in the specified
      `NSUbiquitousKeyValueStore`, defaulting to `defaultValue`.
 
      Values stored will be processed by the provided transformer before being persisted and after being
@@ -223,6 +405,36 @@ extension Persisted {
             storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
             transformer: aggregateTransformer,
             defaultValue: defaultValue(),
+            defaultValuePersistBehaviour: defaultValuePersistBehaviour
+        )
+    }
+
+    /**
+     Create a new instance that stores the value against the `key`,  storing values in the specified
+     `NSUbiquitousKeyValueStore`, defaulting to `wrappedValue`.
+
+     Values stored will be processed by the provided transformer before being persisted and after being
+     retrieved from the storage.
+
+     - parameter wrappedValue: The value to use when a value has not yet been stored, or an error occurs.
+     - parameter key: The key to store the value against
+     - parameter nsUbiquitousKeyValueStore: The store to use to persist and retrieve the value.
+     - parameter transformer: A transformer to transform the value before being persisted and after being retrieved from the storage
+     - parameter defaultValuePersistBehaviour: An option set that describes when to persist the default value. Defaults to `[]`.
+     */
+    public init<Transformer: Persist.Transformer, WrappedValue>(
+        wrappedValue: Value,
+        key: String,
+        nsUbiquitousKeyValueStore: NSUbiquitousKeyValueStore,
+        transformer: Transformer,
+        defaultValuePersistBehaviour: DefaultValuePersistOption = []
+    ) where Transformer.Input == WrappedValue, Transformer.Output: StorableInNSUbiquitousKeyValueStore, Value == WrappedValue? {
+        let aggregateTransformer = transformer.append(transformer: StorableInNSUbiquitousKeyValueStoreTransformer<Transformer.Output>())
+        self.init(
+            key: key,
+            storedBy: NSUbiquitousKeyValueStoreStorage(nsUbiquitousKeyValueStore: nsUbiquitousKeyValueStore),
+            transformer: aggregateTransformer,
+            defaultValue: wrappedValue,
             defaultValuePersistBehaviour: defaultValuePersistBehaviour
         )
     }
