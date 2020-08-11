af709436b15eb8241c1c8107f16a755779570ecf
Joseph Duffy <commits@josephduffy.co.uk>
2020-08-11 18:13:46 +0100

    Add separate package manifest for Swift 5.3
    Allows supporting iOS 8 on Swift 5.2 while fixing deployment target warning in Xcode 12 (which uses Swift 5.3)

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "Persist",
     platforms: [
-        .iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2),
+        .iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2),
     ],
     products: [
         .library(name: "Persist", targets: ["Persist"]),
