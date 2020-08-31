586f3186aabaf66d09944723072fb91f31b47686
Joseph Duffy <commits@josephduffy.co.uk>
2020-08-31 23:02:23 +0100

    Remove public API for StorableInNSUbiquitousKeyValueStore

--- Sources/Persist/NSUbiquitousKeyValueStore/StorableInUbiquitousKeyValueStore.swift
+++ Sources/Persist/NSUbiquitousKeyValueStore/StorableInUbiquitousKeyValueStore.swift
@@ -5,88 +5,63 @@ import Foundation
  A protocol that indicates that a value can be stored in `NSUbiquitousKeyValueStore`. This protocol is
  used to provide type safety and should not be conformed to outside of Persist.
  */
-public protocol StorableInNSUbiquitousKeyValueStore {
+public protocol StorableInNSUbiquitousKeyValueStore {}
+
+internal protocol InternalStorableInNSUbiquitousKeyValueStore: StorableInNSUbiquitousKeyValueStore {
     /// The value of `self` cast to `NSUbiquitousKeyValueStoreValue`.
     var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue { get }
-
-    /// Do not implement this protocol or retrieve this value (it will crash your program). It is for internal use only.
-    var doNotImplementStorableInNSUbiquitousKeyValueStore: Never { get }
 }
 
-extension String: StorableInNSUbiquitousKeyValueStore {
+extension String: InternalStorableInNSUbiquitousKeyValueStore {
     /// An `NSUbiquitousKeyValueStoreValue.string` wrapping `self`.
-    public var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
+    internal var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
         return .string(self)
     }
-
-    public var doNotImplementStorableInNSUbiquitousKeyValueStore: Never {
-        fatalError(#function + " should not be called")
-    }
 }
 
-extension Data: StorableInNSUbiquitousKeyValueStore {
+extension Data: InternalStorableInNSUbiquitousKeyValueStore {
     /// An `NSUbiquitousKeyValueStoreValue.data` wrapping `self`.
-    public var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
+    internal var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
         return .data(self)
     }
-
-    public var doNotImplementStorableInNSUbiquitousKeyValueStore: Never {
-        fatalError(#function + " should not be called")
-    }
 }
 
-extension Bool: StorableInNSUbiquitousKeyValueStore {
+extension Bool: InternalStorableInNSUbiquitousKeyValueStore {
     /// An `NSUbiquitousKeyValueStoreValue.bool` wrapping `self`.
-    public var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
+    internal var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
         return .bool(self)
     }
-
-    public var doNotImplementStorableInNSUbiquitousKeyValueStore: Never {
-        fatalError(#function + " should not be called")
-    }
 }
 
-extension Int64: StorableInNSUbiquitousKeyValueStore {
+extension Int64: InternalStorableInNSUbiquitousKeyValueStore {
     /// An `NSUbiquitousKeyValueStoreValue.int64` wrapping `self`.
-    public var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
+    internal var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
         return .int64(self)
     }
-
-    public var doNotImplementStorableInNSUbiquitousKeyValueStore: Never {
-        fatalError(#function + " should not be called")
-    }
 }
 
-extension Double: StorableInNSUbiquitousKeyValueStore {
+extension Double: InternalStorableInNSUbiquitousKeyValueStore {
     /// An `NSUbiquitousKeyValueStoreValue.double` wrapping `self`.
-    public var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
+    internal var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
         return .double(self)
     }
-
-    public var doNotImplementStorableInNSUbiquitousKeyValueStore: Never {
-        fatalError(#function + " should not be called")
-    }
 }
 
-extension Array: StorableInNSUbiquitousKeyValueStore where Element: StorableInNSUbiquitousKeyValueStore {
+extension Array: StorableInNSUbiquitousKeyValueStore where Element: StorableInNSUbiquitousKeyValueStore {}
+
+extension Array: InternalStorableInNSUbiquitousKeyValueStore where Element: InternalStorableInNSUbiquitousKeyValueStore {
     /// An `NSUbiquitousKeyValueStoreValue.array` wrapping `self`.
-    public var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
+    internal var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
         return .array(map(\.asNSUbiquitousKeyValueStoreValue))
     }
-
-    public var doNotImplementStorableInNSUbiquitousKeyValueStore: Never {
-        fatalError(#function + " should not be called")
-    }
 }
 
-extension Dictionary: StorableInNSUbiquitousKeyValueStore where Key == String, Value: StorableInNSUbiquitousKeyValueStore {
-    /// An `NSUbiquitousKeyValueStoreValue.dictionary` wrapping `self`.
-    public var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
-        return .dictionary(mapValues(\.asNSUbiquitousKeyValueStoreValue))
-    }
+extension Dictionary: StorableInNSUbiquitousKeyValueStore where Key == String, Value: StorableInNSUbiquitousKeyValueStore {}
 
-    public var doNotImplementStorableInNSUbiquitousKeyValueStore: Never {
-        fatalError(#function + " should not be called")
+extension Dictionary: InternalStorableInNSUbiquitousKeyValueStore where Key == String, Value: InternalStorableInNSUbiquitousKeyValueStore {
+    /// An `NSUbiquitousKeyValueStoreValue.dictionary` wrapping `self`.
+    internal var asNSUbiquitousKeyValueStoreValue: NSUbiquitousKeyValueStoreValue {
+        return .dictionary(mapValues(\.asNSUbiquitousKeyValueStoreValue))
     }
 }
 #endif
