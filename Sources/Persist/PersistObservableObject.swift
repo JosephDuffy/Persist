8b0c03bfb78c2d43815964ae757b522f23619783
Joseph Duffy <commits@josephduffy.co.uk>
2020-12-11 21:42:16 +0000

    Add PersistStorage and PersistObservableObject

---
+++ Sources/Persist/PersistObservableObject.swift
@@ -0,0 +1,30 @@
+#if canImport(SwiftUI)
+import Combine
+import SwiftUI
+
+@available(iOS 13, tvOS 13, watchOS 6, macOS 10.15, *)
+public final class PersistObservableObject<Value>: ObservableObject {
+    @Published
+    public private(set) var value: Value
+
+    internal let persister: Persister<Value>
+
+    private var valueCancellables: Combine.AnyCancellable?
+
+    /**
+     Create a new instance that uses the provided `Persister` to persist and retrieve the value.
+     */
+    public init(persister: Persister<Value>) {
+        self.persister = persister
+        value = persister.retrieveValue()
+        valueCancellables = persister.updatesPublisher.dropFirst().sink { [weak self] result in
+            switch result {
+            case .success(let update):
+                self?.value = update.newValue
+            case .failure:
+                break
+            }
+        }
+    }
+}
+#endif
---
+++ Sources/Persist/PersistStorage.swift
@@ -0,0 +1,28 @@
+#if canImport(SwiftUI)
+import SwiftUI
+
+@available(iOS 13, tvOS 13, watchOS 6, macOS 10.15, *)
+@propertyWrapper
+public struct PersistStorage<Value>: DynamicProperty {
+    public var wrappedValue: Value {
+        get { observableObject.value }
+        nonmutating set {
+            try? observableObject.persister.persist(newValue)
+        }
+    }
+
+    public var projectedValue: Binding<Value> {
+        Binding(
+            get: { wrappedValue },
+            set: { wrappedValue = $0 }
+        )
+    }
+
+    @ObservedObject
+    private var observableObject: PersistObservableObject<Value>
+
+    public init(persister: Persister<Value>) {
+        observableObject = PersistObservableObject(persister: persister)
+    }
+}
+#endif
