76e09be9a4b0ba556f2c282e933a0d6530a25c20
Joseph Duffy <commits@josephduffy.co.uk>
2021-07-24 22:32:19 -0700

    Increase platform target to support Xcode 13 beta 3

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Persist",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6),
+        .iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5),
     ],
     products: [
         .library(name: "Persist", targets: ["Persist"]),
